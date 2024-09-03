923c9081ea86f1022f06aa114b8d2c833357c0ea
MrTetsuTetsu <tessei.yamaguchi.1217@gmail.com>
2024-09-03 22:12:35 +0900

    Create Home.js

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+const Home = () => {
+    // 描画用のhtml
+    return (
+        <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8 px-6">
+            <div className="sm:mx-auto sm:w-full sm:max-w-md">
+                <h1 className="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-900">Home</h1>
+            </div>   
+        </div>
+    );
+    
+};
+  
+export default Home;
