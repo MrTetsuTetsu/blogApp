5642f24b6c2f164a1f9def14f1317a9dec4f7f4b
MrTetsuTetsu <tessei.yamaguchi.1217@gmail.com>
2024-09-13 22:34:29 +0900

    Update urls.py

--- blog_project/users/urls.py
+++ blog_project/users/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path, include
-# from rest_framework.routers import DefaultRouter
-from .views import CustomerCreateView, LoginView, UserProfileView, LogoutView, UserUpdateView, csrf_token
+from rest_framework.routers import DefaultRouter
+from .views import CustomerCreateView, LoginView, CustomerProfileView, ChangePasswordView, LogoutView, CustomerUpdateView, csrf_token, DeleteCustomerView
+
+
+router = DefaultRouter()
+router.register(r'create', CustomerCreateView)
 
 urlpatterns = [
-    path('create/', CustomerCreateView.as_view(), name='CreateCustomer'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('profile/', UserProfileView.as_view(), name='profile'),
-    path('update/', UserUpdateView.as_view(), name='profile'),
-    path('csrf-token/', csrf_token, name='csrf_token'),
+  path('', include(router.urls)),
+  path("login/", LoginView.as_view(), name='login'),
+  path('logout/', LogoutView.as_view(), name='logout'),
+  path('profile/', CustomerProfileView.as_view(), name='profile'),
+  path('update/', CustomerUpdateView.as_view(), name='profile'),
+  path('change-password/', ChangePasswordView.as_view()),
+  path('delete/', DeleteCustomerView.as_view()),
+  path('csrf-token/', csrf_token, name='csrf_token'),
 ]
