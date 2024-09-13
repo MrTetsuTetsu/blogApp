633af82f0fe8ce20cce8f1958356b5c352012253
MrTetsuTetsu <tessei.yamaguchi.1217@gmail.com>
2024-09-13 22:30:07 +0900

    Update LoginForms.js

--- frontend/src/components/Customer/LoginForms.js
+++ frontend/src/components/Customer/LoginForms.js
@@ -1,11 +1,13 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../../hooks/AuthContext';
 
 const LoginForm = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-    const [message, setMessage] = useState('');
+    const [message, setMessage] = useState(null);
+    const [messageStatus, setMessageStatus] = useState('');
+    const [showMessage, setShowMessage] = useState(false);
     const navigate = useNavigate();
     const { login } = useAuth();
 
@@ -15,22 +17,27 @@ const LoginForm = () => {
             await login(username, password);
             navigate('/');
         } catch (error) {
-            console.error('Login error:', error);
-            const errorMessage = error.response && error.response.data && error.response.data.error
-                ? error.response.data.error
-                : 'エラーが発生しました。再度お試しください。';
-            setMessage(errorMessage);
+            setMessage('ユーザー名かパスワードが間違っています');
+            setMessageStatus('error');
+            setShowMessage(false); // メッセージを一旦非表示に
+            setTimeout(() => setShowMessage(true), 100); // 少し遅らせて再表示
         }
     };
 
+    const handleDeleteMessage = () => {
+        setMessage(null);
+        setMessageStatus(null);
+        setShowMessage(false);
+    };
+
     return (
-        <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8 px-6">
+        <div className=" min-h-screen translate-y-2 bg-white flex flex-col justify-center sm:px-6 lg:px-8 px-6">
             <div className="sm:mx-auto sm:w-full sm:max-w-md">
-                <h1 className="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-900 pt-12">ログイン</h1>
+                <h1 className="text-center text-3xl leading-9 font-extrabold text-gray-900 mt-5">ログイン</h1>
             </div>
 
-            <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-                <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+            <div className="mt-3 sm:mx-auto sm:w-full sm:max-w-md relative">
+                <div className="bg-white py-4 px-4 shadow sm:rounded-lg sm:px-10">
                     <form onSubmit={handleSubmit}>
                         <div>
                             <label className="block text-sm font-medium leading-5 text-gray-700">Username</label>
@@ -73,8 +80,30 @@ const LoginForm = () => {
                             </span>
                         </div>
                     </form>
-                    {message && <p className="mt-4 text-center text-sm text-red-500">{message}</p>}
                 </div>
+                {showMessage && message && (
+                    <div id="toast-default" className={`absolute flex left-1/2 transform -translate-x-1/2 w-full items-center mt-4 z-50 ${messageStatus === 'success' ? 'text-green-600 bg-green-100' : 'text-red-600 bg-red-100'} animate-fadeInExpand p-2 rounded-md border ${messageStatus === 'success' ? 'border-green-600' : 'border-red-600'}`} role="alert">
+                        <div className={`inline-flex items-center justify-center flex-shrink-0 w-8 h-8 rounded-lg ${messageStatus === 'success' ? 'text-green-500 bg-green-100' : 'text-red-500 bg-red-100'}`}>
+                            {messageStatus === 'success' ? (
+                                <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" />
+                                </svg>
+                            ) : (
+                                <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
+                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15.147 15.085a7.159 7.159 0 0 1-6.189 3.307A6.713 6.713 0 0 1 3.1 15.444c-2.679-4.513.287-8.737.888-9.548A4.373 4.373 0 0 0 5 1.608c1.287.953 6.445 3.218 5.537 10.5 1.5-1.122 2.706-3.01 2.853-6.14 1.433 1.049 3.993 5.395 1.757 9.117Z"/>
+                                </svg>
+                            )}
+                            <span className="sr-only">{messageStatus === 'success' ? 'Check icon' : 'Fire icon'}</span>
+                        </div>
+                        <div className="ms-1 text-sm font-normal">{message}</div>
+                        <button type="button" onClick={handleDeleteMessage} className="ms-auto -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-default" aria-label="Close">
+                            <span className="sr-only">Close</span>
+                            <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                            </svg>
+                        </button>
+                    </div>
+                )}
             </div>
         </div>
     );
